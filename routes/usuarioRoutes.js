311310b731e69b04a3952bb924b3217970f6bfaf
CarlosYKM <carlos.garcia@ykm.com.mx>
2023-06-27 22:55:48 -0600

    Login
    Modificaciones  importantes en view/auth/login en cuanto pug y css

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,8 +1,8 @@
 import express, { Router }  from "express";
-//import { formularioLogin, formularioRegistro, formularioOlvidePassword } from "../controllers/usuarioController.js";
+import {formularioLogin} from "../controllers/ususarioController.js"
 const router = express.Router();
 
-//router.get('/login', formularioLogin);
+router.get('/login', formularioLogin);
 //router.get('/registro', formularioRegistro )
 //router.get('/olvide-password', formularioOlvidePassword)
 export default router
